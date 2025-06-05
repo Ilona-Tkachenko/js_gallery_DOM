002a82d780f1a029113e71811e3ef40e0697b467
Ilona-Tkachenko <ilonatkachenko796@gmail.com>
2025-06-05 20:04:21 +0300

    make build

---
+++ index.e6cb076d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var l=e.target.closest(".list-item").querySelector(".list-item__link");t.src=l.href,t.alt=l.title});
+//# sourceMappingURL=index.e6cb076d.js.map
